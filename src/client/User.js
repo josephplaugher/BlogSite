10176e185c3f0cc898980d4bf5e0977f3a8a1b66
Joseph <joseph@appreciateco.com>
2019-03-13 05:53:11 -0700

    added the light box. branching to add reactform-appco

--- src/client/User.js
+++ src/client/User.js
@@ -1,77 +1,87 @@
-import React from 'react';
-import GoogleAuth from './GoogleAuth'
-import SignIn from './SignIn'
-import NewUser from './NewUser'
-import UserMenu from './UserMenu'
-import SetUrl from './util/SetUrl'
-import 'css/lightbox.css';
-import 'css/form.css';
-import 'css/usermenu.css';
+import React from "react";
+import LightBox from "lightbox-appco";
+import SignIn from "./SignIn";
+import NewUser from "./NewUser";
+import UserMenu from "./UserMenu";
+import SetUrl from "./util/SetUrl";
+import "css/lightbox.css";
+import "css/form.css";
+import "css/usermenu.css";
 
 class User extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {showUserMenu: false};
-        this.showUserMenu = this.showUserMenu.bind(this);
-        this.hideUserMenu = this.hideUserMenu.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = { showUserMenu: false };
+    this.showUserMenu = this.showUserMenu.bind(this);
+    this.hideUserMenu = this.hideUserMenu.bind(this);
+  }
 
-    showUserMenu = () => {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-        this.setState({showUserMenu: true});
-    }
+  showUserMenu = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+    this.setState({ showUserMenu: true });
+  };
 
-    hideUserMenu = () => {
-        this.setState({showUserMenu: false});
-    }
-    render() {
-        const url = SetUrl();
-        
-        return (
-            <div>
-            {this.props.user.username ? (
-                <div>
-                    <div>
-                        <p id="userstate" className="user-menu-area">Signed in as {this.props.user.username}</p>
-                        <div id="user-menu-button" className="user-menu-area" onClick={this.showUserMenu}>
-                            <div className="user-menu-bar"></div>
-                            <div className="user-menu-bar"></div>
-                        </div>
-                    </div>
-             
-                    {this.state.showUserMenu ? (
-                        <div>
-                        <UserMenu 
-                            hideMenu={this.hideUserMenu}
-                            logout={this.props.logout}
-                            user={this.props.user}
-                            updateSubscribed={this.props.updateSubscribed}
-                        />
-                        </div>
-                    ) : (null)}
-                </div>
-            ) : (
-            <div>
-            <div className="lightbox-foundation">
-            {/*this div intentionally empty. Allows the light box content to scroll */}     
-            </div>  
-            <div className="lightbox-background">
-                <div className="lightbox">
-                    <span className="close-button" onClick={this.props.close}>x</span>
-                    <div>
-                    {this.props.authForm === 'sign-in' ? ( <SignIn response={this.props.signInResponse} userNotify={this.props.userNotify}/> )
-                    : this.props.authForm === 'new-user' ? ( <NewUser url={url} response={this.props.signInResponse} userNotify={this.props.userNotify}/> )
-                    : this.props.authForm === 'google' ? ( <GoogleAuth url={url} response={this.props.signInResponse} userNotify={this.props.userNotify}/> ) 
-                    : null
-                    }
-                    </div>
-                </div>
+  hideUserMenu = () => {
+    this.setState({ showUserMenu: false });
+  };
+  render() {
+    const url = SetUrl();
+
+    return (
+      <div>
+        {this.props.user.username ? (
+          <>
+            <p id="userstate" className="user-menu-area">
+              Signed in as {this.props.user.username}
+            </p>
+            <div
+              id="user-menu-button"
+              className="user-menu-area"
+              onClick={this.showUserMenu}
+            >
+              <div className="user-menu-bar" />
+              <div className="user-menu-bar" />
             </div>
-            </div>
-            )}
-            </div>
-        )
-    }
+
+            {this.state.showUserMenu ? (
+              <UserMenu
+                hideMenu={this.hideUserMenu}
+                logout={this.props.logout}
+                user={this.props.user}
+                updateSubscribed={this.props.updateSubscribed}
+              />
+            ) : null}
+          </>
+        ) : (
+          <>
+            <LightBox
+              close={this.props.close}
+              style={{
+                backgroundColor: "white",
+                width: "275px",
+                height: "400px"
+              }}
+            >
+              <div>
+                {this.props.authForm === "sign-in" ? (
+                  <SignIn
+                    response={this.props.signInResponse}
+                    userNotify={this.props.userNotify}
+                  />
+                ) : this.props.authForm === "new-user" ? (
+                  <NewUser
+                    url={url}
+                    response={this.props.signInResponse}
+                    userNotify={this.props.userNotify}
+                  />
+                ) : null}
+              </div>
+            </LightBox>
+          </>
+        )}
+      </div>
+    );
+  }
 }
 
 export default User;
